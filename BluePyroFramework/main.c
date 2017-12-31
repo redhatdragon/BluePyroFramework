b9271628a340111352afdc8dd9b508c3fe3a92af
redhatdragon <paulabrams99@yahoo.com>
2017-12-31 04:33:36 -0500

    Cleaning
    Updated gitignore and modified(and moved) the main.c file.

--- BluePyroFramework/main.c
+++ BluePyroFramework/main.c
@@ -1,15 +1,13 @@
 /*********************************************************************************
 This file may be a .c file however it's being treated like a c++ file.
-This is because we are compiling/linking iwth g++.
+This is because we are compiling/linking with g++.
 So any functions we create here must be declared as "extern "C" int foo()" first.
 This prevents any strange name mangleing.
 *********************************************************************************/
 
-//#include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
 #include <SDL2/SDL.h> 
-//using namespace std;
 
 extern "C" unsigned char BPColorR,BPColorG,BPColorB,BPColorA;
 extern "C" Uint8 BPIsQuit;
@@ -21,16 +19,7 @@ extern "C" void BPRender();
 extern "C" void BPEndGame();
 extern "C" void BPRect(int, int, int, int);
 
-extern "C" int isWPressed();
-int isWPressed() {
-	if (BPKeys[SDL_SCANCODE_W]) {
-		SDLK_w;
-		SDLK_a;
-		SDL_SCANCODE_A;
-		return 1;
-	}
-	return 0;
-}
+
 
 SDL_Window *win;
 SDL_Renderer *ren;
@@ -44,43 +33,28 @@ const Uint8 *BPKeys;
 int BPSetup(){
 	BPKeys = SDL_GetKeyboardState(NULL);
 	if (SDL_Init(SDL_INIT_VIDEO) != 0){
-		//std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
 		printf("SDL_Init Error: ", SDL_GetError());
 		return 1;
 	}
 	win = SDL_CreateWindow("", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
 	if (win == NULL){
-		//std::cout << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
 		printf("SDL_CreateWindow Error: ", SDL_GetError());
 		SDL_Quit();
-		//return 1;
+		return 1;
 	}
 	ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 	SDL_SetRenderDrawBlendMode(ren, SDL_BLENDMODE_BLEND);
 	if (ren == NULL){
 		SDL_DestroyWindow(win);
-		//std::cout << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
 		printf("SDL_CreateRenderer Error: ", SDL_GetError());
 		SDL_Quit();
-		//return 1;
-	}
-	//A sleepy rendering loop, wait for 3 seconds and render and present the screen each time
-	for (int i = 0; i < 3; ++i){
-		//First clear the renderer
-		//SDL_RenderClear(ren);
-		//Draw the texture
-		//SDL_RenderCopy(ren, tex, NULL, NULL);
-		//Update the screen
-		//SDL_RenderPresent(ren);
-		//Take a quick break after all that hard work
-		//SDL_Delay(1000);
+		return 1;
 	}
 	return 0;
 }
 
 void BPClear(){
 	SDL_RenderClear(ren);
-	//SDL_RenderCopy(ren, tex, NULL, NULL);
 	SDL_Rect fillRect = { 0, 0, 800, 600 };
 	SDL_SetRenderDrawColor( ren, 0x00, 0x00, 0x00, 0xFF );        
 	SDL_RenderFillRect( ren, &fillRect );
--- compile.bat
+++ compile.bat
@@ -1,17 +1,17 @@
 echo off
 echo.
 
-echo compiling...
-g++ main.c -c -lmingw32 -mwindows -mconsole^
+echo compiling c++...
+g++ BluePyroFramework\main.c -c -lmingw32 -mwindows -mconsole^
  -L lib -lSDL2main -lSDL2
 echo.
 
 echo compiling asm...
-nasm main.asm -f win32 -o mainAsm.o -I BluePyroGraphics
+nasm main.asm -f win32 -o mainAsm.o
 echo.
 
 echo linking...
-g++ main.c mainAsm.o -o binWIN32\a.exe^
+g++ BluePyroFramework\main.c mainAsm.o -o binWIN32\a.exe^
  -lmingw32 -mwindows -mconsole^
  -L lib -lSDL2main -lSDL2
 echo.
